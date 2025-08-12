ef485f250de303ae0bb47057e92f149065f9ddda
mdraselahmedcode <mdraselahmed.code@gmail.com>
2025-08-13 01:51:29 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -14,7 +14,6 @@ define('BASE_PATH', rtrim($_SERVER['DOCUMENT_ROOT'], '/') . $_ENV['BASE_PATH']);
 define('BASE_URL', rtrim($_ENV['BASE_URL'], '/'));
 
 // Payment currency symbol
-// Payment currency symbol
 if (!defined('CURRENCY_SYMBOL')) {
     define('CURRENCY_SYMBOL', $_ENV['CURRENCY_SYMBOL'] ?? '৳');
 }
